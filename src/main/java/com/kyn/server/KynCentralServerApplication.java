85ffcc77f0dbd471fcbec2c3152e98520f236fc4
Pawan Gupta <pawan-crestron>
2022-09-27 22:51:42 +0530

    First commit

---
+++ src/main/java/com/kyn/server/KynCentralServerApplication.java
@@ -0,0 +1,15 @@
+package com.kyn.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class KynCentralServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KynCentralServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyn/server/KynCentralServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyn.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KynCentralServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
